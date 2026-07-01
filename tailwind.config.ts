ee468e1afb1902a8fd24fc74e54b812f010115ba
ajitbasnet <ajitbasnet32@gmail.com>
2026-07-01 19:22:59 +0545

    Added darkMode:class to tailwind.config.ts; update index.css global + skeleton dark styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,7 @@ import forms from '@tailwindcss/forms'
 
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
